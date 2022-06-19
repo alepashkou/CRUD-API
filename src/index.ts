d6670a262836ef6f17309baf03c4ab74df8ba928
Alexander Pashkou <ale.pashkou@gmail.com>
2022-06-19 13:39:00 +0300

    feat: add two scenarios for test

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,24 @@
 import http from 'http';
 import 'dotenv/config';
-import { User } from './user';
 import { getUsers } from './controllers/getUsers.js';
 import { getUser } from './controllers/getUser.js';
 import { addUser } from './controllers/addUser.js';
 import { updateUser } from './controllers/updateUser.js';
 import { deleteUser } from './controllers/deleteUser.js';
+import mainData from './data/db.js';
 
 const PORT: number = +process.env.PORT || 4000;
-const mainData: User[] = [
-  // {
-  //   id: 'a889e0f7-d734-4e4e-ab66-192047a97c89',
-  //   username: 'John',
-  //   age: 30,
-  //   hobbies: ['Sports', 'Cooking'],
-  // },
-];
+
 export const server: http.Server = http.createServer(
   (req: http.IncomingMessage, res: http.ServerResponse) => {
     const urlArray: string[] = req.url.split('/');
     if (req.url === '/api/users' && req.method === 'GET') {
       getUsers(res, mainData);
-    } else if (urlArray.length < 5 && req.method === 'GET') {
+    } else if (
+      urlArray.length > 3 &&
+      urlArray.length < 5 &&
+      req.method === 'GET'
+    ) {
       getUser(res, mainData, urlArray[3]);
     } else if (req.url === '/api/users' && req.method === 'POST') {
       let data: string = '';
--- tests/server.test.ts
+++ tests/server.test.ts
@@ -1,57 +1,131 @@
 import { server } from '../dist/index.js';
 import request from 'supertest';
+describe('Testing', () => {
+  describe('Scenario 1 (all function)', () => {
+    let userId: string = '';
 
-describe('Scenario 1', () => {
-  let userId = '';
+    test('Should return empty array', async () => {
+      const responce = await request(server).get('/api/users');
+      expect(responce.body).toEqual([]);
+    });
 
-  test('Should return empty array', async () => {
-    const responce = await request(server).get('/api/users');
-    expect(responce.body).toEqual([]);
+    test('Should create user', async () => {
+      const responce = await request(server)
+        .post('/api/users')
+        .send({
+          username: 'John',
+          age: 30,
+          hobbies: ['Sports', 'Cooking'],
+        });
+      expect(responce.statusCode).toBe(201);
+      expect(responce.body['username']).toBe('John');
+      expect(responce.body['age']).toBe(30);
+      expect(responce.body['hobbies']).toEqual(['Sports', 'Cooking']);
+      userId = responce.body['id'];
+    });
+
+    test('Get created user', async () => {
+      const responce = await request(server).get('/api/users/' + userId);
+      expect(responce.statusCode).toBe(200);
+      expect(responce.body['id']).toBe(userId);
+      expect(responce.body['username']).toBe('John');
+      expect(responce.body['age']).toBe(30);
+    });
+
+    test('Update user', async () => {
+      const responce = await request(server)
+        .put('/api/users/' + userId)
+        .send({
+          username: 'Johnik',
+        });
+      expect(responce.statusCode).toBe(200);
+    });
+
+    test('Delete created user', async () => {
+      const responce = await request(server).delete('/api/users/' + userId);
+      expect(responce.statusCode).toBe(204);
+    });
+
+    test('Check deleted user', async () => {
+      const responce = await request(server).get('/api/users');
+      expect(responce.statusCode).toBe(200);
+      expect(responce.body).toEqual([]);
+    });
   });
-
-  test('Should create user', async () => {
-    const responce = await request(server)
-      .post('/api/users')
-      .send({
-        username: 'John',
+  describe('Scenario 2 (create many users)', () => {
+    let usersIds: string[] = [];
+    test('Create users (1000)', async () => {
+      for (let i = 0; i <= 1000; i++) {
+        const responce = await request(server)
+          .post('/api/users')
+          .send({
+            username: 'Test ' + i,
+            age: 30,
+            hobbies: [],
+          });
+        expect(responce.statusCode).toBe(201);
+        usersIds.push(responce.body['id']);
+      }
+    });
+    test('Delete users (1000)', async () => {
+      for (let i = 0; i <= 1000; i++) {
+        const responce = await request(server).delete(
+          '/api/users/' + usersIds[i]
+        );
+        expect(responce.statusCode).toBe(204);
+      }
+    });
+  });
+  describe('Scenario 3 (handle errors)', () => {
+    test('Non-existing endpoint (404)', async () => {
+      const responce = await request(server).get('/some-non');
+      expect(responce.statusCode).toBe(404);
+    });
+    test('User id not valid (400)', async () => {
+      const responce = await request(server).get('/api/users/123123123adsdsa');
+      expect(responce.statusCode).toBe(400);
+    });
+    test('User not found (404)', async () => {
+      const responce = await request(server).get(
+        '/api/users/a889e0f7-d734-4e4e-ab66-192047a97c89'
+      );
+      expect(responce.statusCode).toBe(404);
+    });
+    test('Create user not required fields (400)', async () => {
+      const responce = await request(server).post('/api/users').send({
         age: 30,
-        hobbies: ['Sports', 'Cooking'],
       });
-    expect(responce.statusCode).toBe(201);
-    expect(responce.body['username']).toBe('John');
-    expect(responce.body['age']).toBe(30);
-    expect(responce.body['hobbies']).toEqual(['Sports', 'Cooking']);
-    userId = responce.body['id'];
+      expect(responce.statusCode).toBe(400);
+    });
+    test('Update user id not valid (400)', async () => {
+      const responce = await request(server)
+        .put('/api/users/123123123adsdsa')
+        .send({});
+      expect(responce.statusCode).toBe(400);
+    });
+    test('Update user id not found (404)', async () => {
+      const responce = await request(server)
+        .put('/api/users/a889e0f7-d734-4e4e-ab66-192047a97c89')
+        .send({});
+      expect(responce.statusCode).toBe(404);
+    });
+    test('Delete user id not valid (400)', async () => {
+      const responce = await request(server).delete(
+        '/api/users/123123123adsdsa'
+      );
+      expect(responce.statusCode).toBe(400);
+    });
+    test('Delete user id not found (404)', async () => {
+      const responce = await request(server).delete(
+        '/api/users/a889e0f7-d734-4e4e-ab66-192047a97c89'
+      );
+      expect(responce.statusCode).toBe(404);
+    });
+    test('Server error (500)', async () => {
+      const responce = await request(server).post('/api/users').send(undefined);
+      expect(responce.statusCode).toBe(500);
+    });
   });
-
-  test('Get created user', async () => {
-    const responce = await request(server).get('/api/users/' + userId);
-    expect(responce.statusCode).toBe(200);
-    expect(responce.body['id']).toBe(userId);
-    expect(responce.body['username']).toBe('John');
-    expect(responce.body['age']).toBe(30);
-  });
-
-  test('Update user', async () => {
-    const responce = await request(server)
-      .put('/api/users/' + userId)
-      .send({
-        username: 'Johnik',
-      });
-    expect(responce.statusCode).toBe(200);
-  });
-
-  test('Delete created user', async () => {
-    const responce = await request(server).delete('/api/users/' + userId);
-    expect(responce.statusCode).toBe(204);
-  });
-
-  test('Check deleted user', async () => {
-    const responce = await request(server).get('/api/users');
-    expect(responce.statusCode).toBe(200);
-    expect(responce.body).toEqual([]);
-  });
-
   afterAll(() => {
     server.close();
   });
